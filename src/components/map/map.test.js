ee47e59aac21c11a077becc4eb6984f931066929
David Papamichael <davidpapamichael@gmail.com>
2020-04-08 20:59:22 +0100

    snapshots updated

--- src/components/map/map.test.js
+++ src/components/map/map.test.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import {ReactDOM, unmountComponentAtNode} from "react-dom";
+import { ReactDOM, unmountComponentAtNode } from "react-dom";
 import { render } from '@testing-library/react';
 import { shallow, simulate } from 'enzyme';
-import {MapContainer, Map} from './map'
+import { MapContainer, Map } from './map'
 import Marker from 'google-maps-react'
 import renderer from 'react-test-renderer';
 import MockedMap from './map'
@@ -10,9 +10,9 @@ import { act } from "react-dom/test-utils"
 
 
 
-it('does something on marker click', () => {    
+it('does something on marker click', () => {
   const onMarkerClick = jest.fn();
-  const component = shallow(<MapContainer countries={[]} onMarkerClick={onMarkerClick}/>); 
+  const component = shallow(<MapContainer countries={[]} onMarkerClick={onMarkerClick} />);
   const marker = component.find(Marker)
   expect(marker.simulate('click').first().length).toEqual(1)
 });
