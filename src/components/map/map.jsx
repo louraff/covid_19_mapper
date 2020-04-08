6b78c3bc82983295b3633ab3d328222f2e53ecf8
nicolasraffray <nraffray@gmail.com>
2020-04-08 16:51:03 +0100

    getting country id from clicking on circle

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,38 +1,75 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap, Marker, Circle } from "react-google-maps"
+import { withGoogleMap, GoogleMap, InfoWindow, Marker, Circle } from "react-google-maps"
 import styles from './../assets/mapStyle.json'
 
 class MapContainer extends Component {
-  state = {  }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      center: { lat: 40.4929, lng: 15.5553 },
+      isMarkerShowing: true,
+      selectedPlace: {},
+      activeMarker: {}, 
+    }
+  }
 
+  onMarkerClicked = (marker) => {
+    console.log("marker", marker)
+    // this.setState({
+    //   isMarkerShowing: true,
+    //   activeMarker: marker,
+    // });
+  };
 
+  onCircleClicked = (props) => {
+    console.log(props)
+  }
+  
   render() { 
-    console.log(styles)
-    const GoogleMapExample = withGoogleMap(props => (
+    console.log(this.props)
+    const GoogleMapExample = withGoogleMap((props) => (
       <GoogleMap
-        defaultCenter = { { lat: 40.4929, lng: 15.5553 } }
-        defaultZoom = { 2 }
-        defaultOptions = {{ styles: styles}}
-      ><Circle
-            defaultCenter={{ lat: 40.4929, lng: 15.5553 }}
-            radius={100000}
-            options= {{
-              strokeColor: "#ff0000",
-              strokeOpacity: 0.8,
-              strokeWeight: 0,
-              fillColor: '#FF0000',
-              fillOpacity: 0.3
-            }}
-              />
+        defaultCenter={this.state.center}
+        defaultZoom = { 2.25 }
+        options = {{
+          disableDefaultUI: true,
+          styles: styles,
+          minZoom: 2,
+          maxZoom: 10,
+          zoomControl: true
+        }}
+      >
+        {this.props.countries.map((country, i) => (
+         <Circle
+          // key={i}
+          defaultCenter={ country.center}
+          radius={100000}
+          onClick={ () => this.onMarkerClicked(i)}
+          options= {{
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 0,
+            fillColor: '#FF0000',
+            fillOpacity: 0.3
+          }}>
+            <InfoWindow
+            key={i}
+            marker={this.state.activeMarker}
+            visible={this.state.isMarkerShowing}
+          ></InfoWindow>
+           </Circle>
+         
+        ))}
+          
       </GoogleMap>
-    ));  
-
+    ));
+    
 
     return ( <div>
         <GoogleMapExample
-          containerElement={ <div style={{ height: '1000px', width: '1000px' }} /> }
-          mapElement={ <div style={{ height: `100%`, width: '100%' }} /> }
+          containerElement={ <div id="container" /> }
+          mapElement={ <div id="map" /> }
         />
       </div> );
   }
