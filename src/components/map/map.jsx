510cb8b3932a2c5548e049e463d77d31f00fbdf0
David Papamichael <davidpapamichael@gmail.com>
2020-04-08 20:51:28 +0100

    states changed to blue circles, countries to red circles

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -12,10 +12,9 @@ class MapContainer extends Component {
       center: { lat: 40.4929, lng: 15.5553 },
       isMarkerShowing: true,
       selectedPlace: {},
-      activeMarker: {}, 
+      activeMarker: {},
     }
   }
-
 
   onMarkerClicked = (marker) => {
     this.countryElement.current.changeCountry(marker)
@@ -24,14 +23,13 @@ class MapContainer extends Component {
   onCircleClicked = (props) => {
     console.log(props)
   }
-  
-  render() { 
-    console.log(this.props)
+
+  render() {
     const GoogleMapExample = withGoogleMap((props) => (
       <GoogleMap
         defaultCenter={this.state.center}
-        defaultZoom = { 2.25 }
-        options = {{
+        defaultZoom={2.25}
+        options={{
           disableDefaultUI: true,
           styles: styles,
           minZoom: 2,
@@ -40,41 +38,47 @@ class MapContainer extends Component {
         }}
       >
         {this.props.countries.map((country, i) => (
-         <Circle
-          // key={i}
-          defaultCenter={ country.center}
-          radius={100000}
-          onClick={ () => this.onMarkerClicked(country.country)}
-          options= {{
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 0,
-            fillColor: '#FF0000',
-            fillOpacity: 0.3
-          }}>
-           </Circle>
-         
+          <Circle
+            // key={i}
+            defaultCenter={country.center}
+            radius={100000}
+            onClick={() => this.onMarkerClicked(country.country)}
+            options={country.us ?
+              {
+                fillColor: '#007aa3',
+                fillOpacity: 0.4,
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.8,
+                strokeWeight: 0
+              }
+              : {
+                fillColor: '#FF0000',
+                fillOpacity: 0.3,
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.8,
+                strokeWeight: 0
+              }
+            }
+          >
+          </Circle>
         ))}
-          
       </GoogleMap>
-
     ));
-    
 
-    return ( <div>
-
+    return (
       <div>
-        <GoogleMapExample
-          containerElement={ <div id="container" /> }
-          mapElement={ <div id="map" /> }
-        />
+        <div>
+          <GoogleMapExample
+            containerElement={<div id="container" />}
+            mapElement={<div id="map" />}
+          />
         </div>
         <div id="mapinfo">
-          <MapInfo ref={this.countryElement} countriesArray={this.props.countries}/> 
+          <MapInfo ref={this.countryElement} countriesArray={this.props.countries} />
         </div>
-      </div> 
+      </div>
     );
   }
 }
- 
+
 export default MapContainer;
