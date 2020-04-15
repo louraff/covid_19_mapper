97d21fb3c5d28842f8d3d733408ecf5bd02faa43
Asia <asiaellis5@gmail.com>
2020-04-15 17:51:36 +0100

    more styling and tests

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,35 +1,34 @@
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton"
+import DropdownButton from "react-bootstrap/DropdownButton";
 import Nav from "react-bootstrap/Nav";
 import Spinner from "react-bootstrap/Spinner";
 import MapContainer from "./../map/map";
-import TableContainer from "./../tables/table"
+import TableContainer from "./../tables/table";
 
 class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      viewMap: false,
-      viewTables: true
-    }
+      viewMap: true,
+      viewTables: false,
+    };
   }
 
   handleMapClick = () => {
     this.setState({
       viewMap: true,
-      viewTables: false
-    })
-  }
+      viewTables: false,
+    });
+  };
 
   handleTableClick = () => {
     this.setState({
       viewMap: false,
-      viewTables: true
-    })
-  }
-
+      viewTables: true,
+    });
+  };
 
   render() {
     return (
@@ -40,8 +39,8 @@ class Header extends Component {
               variant={"outline-light"}
               title={
                 "Global Recoveries: " + this.props.total.total_recovered ===
-                  "Global Recoveries: undefined"
-                  ? "Last Updated" && (
+                "Global Recoveries: undefined" ? (
+                  "Last Updated" && (
                     <Spinner
                       as="span"
                       animation="grow"
@@ -50,7 +49,8 @@ class Header extends Component {
                       aria-hidden="true"
                     />
                   )
-                  : <span>
+                ) : (
+                  <span>
                     <span id="img">
                       <img
                         alt=""
@@ -58,26 +58,41 @@ class Header extends Component {
                         width="22"
                         height="22"
                         className="d-inline-block align-top"
-                      ></img></span>
-                 Covid-19
-                </span>
+                      ></img>
+                    </span>
+                    Covid-19
+                  </span>
+                )
               }
               id="last_updated"
             >
               <Dropdown.Item onClick={this.handleMapClick}>
                 <div id="drop-down-window">
                   <div id="info-icon">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Globe_icon.svg/420px-Globe_icon.svg.png" height="18px" width="18px" alt=""></img>
+                    <img
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Globe_icon.svg/420px-Globe_icon.svg.png"
+                      height="18px"
+                      width="18px"
+                      alt=""
+                    ></img>
                   </div>
-
-                  View Map </div></Dropdown.Item>
+                  View Map{" "}
+                </div>
+              </Dropdown.Item>
 
               <Dropdown.Item onClick={this.handleTableClick}>
                 <div id="drop-down-window">
                   <div id="info-icon">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Simple_icon_table.svg/1280px-Simple_icon_table.svg.png" height="16px" width="20px" alt=""></img>
+                    <img
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Simple_icon_table.svg/1280px-Simple_icon_table.svg.png"
+                      height="16px"
+                      width="20px"
+                      alt=""
+                    ></img>
                   </div>
-                  View Tables</div></Dropdown.Item>
+                  View Tables
+                </div>
+              </Dropdown.Item>
             </DropdownButton>
           </Nav>
           <Nav className="ml-auto">
@@ -85,16 +100,16 @@ class Header extends Component {
               variant={"outline-info"}
               title={
                 "Global Cases: " + this.props.total.total_cases ===
-                  "Global Cases: undefined"
+                "Global Cases: undefined"
                   ? "Global Cases: " && (
-                    <Spinner
-                      as="span"
-                      animation="grow"
-                      size="sm"
-                      role="status"
-                      aria-hidden="true"
-                    />
-                  )
+                      <Spinner
+                        as="span"
+                        animation="grow"
+                        size="sm"
+                        role="status"
+                        aria-hidden="true"
+                      />
+                    )
                   : "Global Cases: " + this.props.total.total_cases
               }
               id="last_updated"
@@ -102,7 +117,7 @@ class Header extends Component {
               <Dropdown.Item>
                 <div id="drop-down-window">
                   <Spinner animation="grow" variant="info" size="sm" />
-              Daily Increase: {this.props.total.new_cases}
+                  Daily Increase: {this.props.total.new_cases}
                 </div>
               </Dropdown.Item>
             </DropdownButton>
@@ -112,16 +127,16 @@ class Header extends Component {
               variant={"outline-success"}
               title={
                 "Global Recoveries: " + this.props.total.total_recovered ===
-                  "Global Recoveries: undefined"
+                "Global Recoveries: undefined"
                   ? "Global Recoveries: " && (
-                    <Spinner
-                      as="span"
-                      animation="grow"
-                      size="sm"
-                      role="status"
-                      aria-hidden="true"
-                    />
-                  )
+                      <Spinner
+                        as="span"
+                        animation="grow"
+                        size="sm"
+                        role="status"
+                        aria-hidden="true"
+                      />
+                    )
                   : "Global Recoveries: " + this.props.total.total_recovered
               }
               id="last_updated"
@@ -129,7 +144,7 @@ class Header extends Component {
               <Dropdown.Item>
                 <div id="drop-down-window">
                   <Spinner animation="grow" variant="success" size="sm" />
-                Active Cases: {this.props.total.active_cases}
+                  Active Cases: {this.props.total.active_cases}
                 </div>
               </Dropdown.Item>
             </DropdownButton>
@@ -139,16 +154,16 @@ class Header extends Component {
               variant={"outline-danger"}
               title={
                 "Global Deaths: " + this.props.total.total_deaths ===
-                  "Global Deaths: undefined"
+                "Global Deaths: undefined"
                   ? "Global Deaths: " && (
-                    <Spinner
-                      as="span"
-                      animation="grow"
-                      size="sm"
-                      role="status"
-                      aria-hidden="true"
-                    />
-                  )
+                      <Spinner
+                        as="span"
+                        animation="grow"
+                        size="sm"
+                        role="status"
+                        aria-hidden="true"
+                      />
+                    )
                   : "Global Deaths: " + this.props.total.total_deaths
               }
               id="last_updated"
@@ -156,7 +171,7 @@ class Header extends Component {
               <Dropdown.Item>
                 <div id="drop-down-window">
                   <Spinner animation="grow" variant="danger" size="sm" />
-                Daily Increase: {this.props.total.new_deaths}
+                  Daily Increase: {this.props.total.new_deaths}
                 </div>
               </Dropdown.Item>
             </DropdownButton>
@@ -166,27 +181,34 @@ class Header extends Component {
               alignRight
               variant={"outline-warning"}
               title={
-                "Global C.F.R: " + this.props.globalCFR ===
-                  "Global C.F.R: null"
+                "Global C.F.R: " + this.props.globalCFR === "Global C.F.R: null"
                   ? "Global C.F.F: " && (
-                    <Spinner
-                      as="span"
-                      animation="grow"
-                      size="sm"
-                      role="status"
-                      aria-hidden="true"
-                    />
-                  )
-                  : "Global C.F.R: " + this.props.globalCFR + '%'
+                      <Spinner
+                        as="span"
+                        animation="grow"
+                        size="sm"
+                        role="status"
+                        aria-hidden="true"
+                      />
+                    )
+                  : "Global C.F.R: " + this.props.globalCFR + "%"
               }
               id={"dropdown-menu-align-right"}
             >
               <Dropdown.Item>
                 <div id="drop-down-window">
                   <div id="info-icon">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Infobox_info_icon.svg/1200px-Infobox_info_icon.svg.png" height="22px" width="22px" alt=""></img>
+                    <img
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Infobox_info_icon.svg/1200px-Infobox_info_icon.svg.png"
+                      height="22px"
+                      width="22px"
+                      alt=""
+                    ></img>
                   </div>
-                  <div>Case Fatality Rate = % of Recorded Cases that Result in Deaths</div>
+                  <div>
+                    Case Fatality Rate = % of Recorded Cases that Result in
+                    Deaths
+                  </div>
                 </div>
               </Dropdown.Item>
             </DropdownButton>
@@ -197,16 +219,16 @@ class Header extends Component {
               variant={"outline-secondary"}
               title={
                 "Global Recoveries: " + this.props.total.total_recovered ===
-                  "Global Recoveries: undefined"
+                "Global Recoveries: undefined"
                   ? "Last Updated" && (
-                    <Spinner
-                      as="span"
-                      animation="grow"
-                      size="sm"
-                      role="status"
-                      aria-hidden="true"
-                    />
-                  )
+                      <Spinner
+                        as="span"
+                        animation="grow"
+                        size="sm"
+                        role="status"
+                        aria-hidden="true"
+                      />
+                    )
                   : "Last Updated"
               }
               id={"dropdown-menu-align-right"}
@@ -214,14 +236,13 @@ class Header extends Component {
               <Dropdown.Item>
                 <div id="drop-down-window">
                   <Spinner animation="grow" variant="secondary" size="sm" />
-                UTC: {this.props.total.statistic_taken_at}
+                  UTC: {this.props.total.statistic_taken_at}
                 </div>
               </Dropdown.Item>
             </DropdownButton>
           </Nav>
         </Navbar>
-        {
-          this.state.viewMap &&
+        {this.state.viewMap && (
           <div className="Container">
             <MapContainer
               countries={this.props.countries}
@@ -230,15 +251,13 @@ class Header extends Component {
               integerCountries={this.props.integerCountries}
             />
           </div>
-        }
-        {
-          this.state.viewTables &&
+        )}
+        {this.state.viewTables && (
           <div id="table">
             <TableContainer countries={this.props.integerCountries} />
           </div>
-        }
-
-      </div >
+        )}
+      </div>
     );
   }
 }
