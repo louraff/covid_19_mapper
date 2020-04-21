059340fa13bb255189602214cdded97868cbf41b
Asia <asiaellis5@gmail.com>
2020-04-21 15:07:43 +0100

    imported chart defaults and changed the font to white

--- src/components/graph/graph.jsx
+++ src/components/graph/graph.jsx
@@ -1,226 +1,229 @@
-import React, { Component } from 'react';
-import { Line, Doughnut } from 'react-chartjs-2'
-
+import React, { Component } from "react";
+import { Line, Doughnut, defaults } from "react-chartjs-2";
 
 class GraphContainer extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      data: []
-    }
+      data: [],
+    };
   }
 
   componentDidMount() {
     fetch("https://pomber.github.io/covid19/timeseries.json")
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         this.setState({
-          data: data
-        })
+          data: data,
+        });
       });
   }
 
   createLabels = () => {
-    const labelData = []
+    const labelData = [];
     if (this.state.data.US !== undefined) {
-      this.state.data.US.map(date => {
-        labelData.push(date.date)
-      })
-      return labelData
+      this.state.data.US.map((date) => {
+        labelData.push(date.date);
+      });
+      return labelData;
     }
-  }
+  };
 
   createGraphData = (type) => {
     const graphData = {
       deaths: [],
-      confirmed: [], 
-      recoveries: []
-    }
-    
-    // let array = []
-    const countryData = this.state.data[this.props.country]
+      confirmed: [],
+      recoveries: [],
+    };
 
+    // let array = []
+    const countryData = this.state.data[this.props.country];
 
     // console.log("filtered array", array)
     if (countryData !== undefined) {
-      countryData.map(date => {
-        graphData.confirmed.push(date.confirmed)
-        graphData.deaths.push(date.deaths)
-        graphData.recoveries.push(date.recovered)
-      // })
-      })
+      countryData.map((date) => {
+        graphData.confirmed.push(date.confirmed);
+        graphData.deaths.push(date.deaths);
+        graphData.recoveries.push(date.recovered);
+        // })
+      });
 
-
-      if (type === 'confirmed') {
-        return graphData.confirmed
+      if (type === "confirmed") {
+        return graphData.confirmed;
       } else if (type === "deaths") {
-        return graphData.deaths
+        return graphData.deaths;
       } else if (type === "recovered") {
-        return graphData.recoveries
+        return graphData.recoveries;
       }
     }
-  }
-
+  };
 
   doughnutLabels = () => {
-    const doughtnutLabels = []
+    const doughtnutLabels = [];
     if (this.props.countries !== undefined) {
-      this.props.countries.map(country => {
-        doughtnutLabels.push(country.country)
-      })
+      this.props.countries.map((country) => {
+        doughtnutLabels.push(country.country);
+      });
     }
-    return doughtnutLabels
-  }
+    return doughtnutLabels;
+  };
 
   doughnutData = () => {
-    const doughnutData = []
+    const doughnutData = [];
     if (this.props.countries !== undefined) {
-      this.props.countries.map(country => {
+      this.props.countries.map((country) => {
         if (!country.us) {
-          doughnutData.push((country.confirmed / this.props.total[0] * 100).toFixed(2))
+          doughnutData.push(
+            ((country.confirmed / this.props.total[0]) * 100).toFixed(2)
+          );
         }
-      })
+      });
     }
-    return doughnutData
-  }
+    return doughnutData;
+  };
 
   render() {
+    console.log(defaults);
+    defaults.global.defaultFontColor = "white";
+
     const line = {
       labels: this.createLabels(),
       datasets: [
         {
-          label: 'Confirmed Cases',
+          label: "Confirmed Cases",
           data: this.createGraphData("confirmed"),
           fill: false,
-          backgroundColor: '#18A2B8',
-          borderColor: '#18A2B8',
+          backgroundColor: "#18A2B8",
+          borderColor: "#18A2B8",
           borderWidth: 2,
-          pointBackgroundColor: '#18A2B8',
-          pointBorderColor: '#000000',
+          pointBackgroundColor: "#18A2B8",
+          pointBorderColor: "#000000",
           pointBorderWidth: 0.5,
-          pointStyle: 'rectRounded',
-          hoverBackgroundColor: '#FFFFFF'
+          pointStyle: "rectRounded",
+          hoverBackgroundColor: "#FFFFFF",
+          defaultFontColor: "red",
         },
         {
-          label: 'Confirmed Deaths',
+          label: "Confirmed Deaths",
           data: this.createGraphData("deaths"),
           fill: false,
-          backgroundColor: '#dc3644',
-          borderColor: '#dc3644',
+          backgroundColor: "#dc3644",
+          borderColor: "#dc3644",
           borderWidth: 2,
-          pointBackgroundColor: '#dc3644',
-          pointBorderColor: '#000000',
+          pointBackgroundColor: "#dc3644",
+          pointBorderColor: "#000000",
           pointBorderWidth: 0.5,
-          pointStyle: 'rectRounded',
-          hoverBackgroundColor: '#FFFFFF',
+          pointStyle: "rectRounded",
+          hoverBackgroundColor: "#FFFFFF",
         },
         {
-          label: 'Confrmed Recoveries',
+          label: "Confrmed Recoveries",
           data: this.createGraphData("recovered"),
           fill: false,
-          backgroundColor: '#28a745',
-          borderColor: '#28a745',
+          backgroundColor: "#28a745",
+          borderColor: "#28a745",
           borderWidth: 2,
-          pointBackgroundColor: '#28a745',
-          pointBorderColor: '#000000',
+          pointBackgroundColor: "#28a745",
+          pointBorderColor: "#000000",
           pointBorderWidth: 0.5,
-          pointStyle: 'rectRounded',
-          hoverBackgroundColor: '#FFFFFF'
-
-        }
+          pointStyle: "rectRounded",
+          hoverBackgroundColor: "#FFFFFF",
+        },
       ],
-    }
+    };
 
     const lOptions = {
       scales: {
-        xAxes: [{
-          ticks: {
-            display: true,
-            major: {
-              fontStyle: 'bold',
-              fontColor: '#FFFFFF'
-            }
+        xAxes: [
+          {
+            ticks: {
+              display: true,
+              major: {
+                fontStyle: "bold",
+                fontColor: "#FFFFFF",
+              },
+            },
+            gridLines: {
+              display: false,
+              drawBorder: true,
+            },
+            scaleLabel: {
+              display: true,
+              labelString: "Date (YYYY/MM/DD)",
+              fontStyle: "bold",
+              fontColor: "#FFFFFF",
+            },
           },
-          gridLines: {
-            display: false,
-            drawBorder: true
+        ],
+        yAxes: [
+          {
+            ticks: {
+              display: true,
+              major: {
+                fontStyle: "bold",
+                fontColor: "#FFFFFF",
+              },
+            },
+            gridLines: {
+              display: false,
+              drawBorder: true,
+            },
+            scaleLabel: {
+              display: true,
+              labelString: "Number of People",
+              fontStyle: "bold",
+              fontColor: "#FFFFFF",
+            },
           },
-          scaleLabel: {
-            display: true,
-            labelString: 'Date (YYYY/MM/DD)',
-            fontStyle: 'bold',
-            fontColor: '#FFFFFF'
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            display: true,
-            major: {
-              fontStyle: 'bold',
-              fontColor: '#FFFFFF'
-            }
-          },
-          gridLines: {
-            display: false,
-            drawBorder: true
-          },
-          scaleLabel: {
-            display: true,
-            labelString: 'People',
-            fontStyle: 'bold',
-            fontColor: '#FFFFFF'
-          }
-        }],
+        ],
       },
       legend: {
         display: true,
-        position: 'top',
-        align: 'center',
+        position: "top",
+        align: "center",
         labels: {
           fontSize: 10,
-          fontStyle: 'bold',
-          fontColor: '#FFFFFF',
-          usePointStyle: true
+          fontStyle: "bold",
+          fontColor: "#FFFFFF",
+          usePointStyle: true,
         },
       },
       lineTension: 3,
-      borderWidth: 2
-    }
+      borderWidth: 2,
+    };
 
     const dOptions = {
       legend: false,
       animation: {
-        animateScale: true
+        animateScale: true,
       },
       cutoutPercentage: 50,
       circumfrance: 0.141596,
       title: {
-        display: true
+        display: true,
       },
       tooltips: {
-        backgroundColor: '#18A2B8',
-        displayColors: false
+        backgroundColor: "#18A2B8",
+        displayColors: false,
       },
       onClick: (evnt, item) => {
-        console.log(evnt)
-        console.log(item[0])
-      }
-        
-      
-    }
+        console.log(item[0]);
+        item[0]._model.outerRadius += 5;
+      },
+    };
 
     const doughnut = {
       labels: this.doughnutLabels(),
-      datasets: [{
-        data: this.doughnutData(),
-        backgroundColor: '#646D79',
-        hoverBackgroundColor: '#18A2B8',
-        borderWidth: 0.1,
-        borderColor: false,
-        
-
-      }]
-    }
+      datasets: [
+        {
+          data: this.doughnutData(),
+          backgroundColor: "#646D79",
+          hoverBackgroundColor: "#18A2B8",
+          borderWidth: 0.1,
+          borderColor: false,
+        },
+      ],
+    };
 
     return (
       <div id="graph" className="graph">
@@ -233,7 +236,7 @@ class GraphContainer extends Component {
           <Doughnut data={doughnut} options={dOptions} />
         </div>
       </div>
-    )
+    );
   }
 }
 
