6db30765d3187e6605da8ccd4a8a9568af9c58f3
Asia <asiaellis5@gmail.com>
2020-06-14 14:15:26 +0100

    added a test for to Integer function

--- src/App.test.js
+++ src/App.test.js
@@ -1,38 +1,34 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-import { shallow } from 'enzyme';
-import App from './App';
-import renderer from 'react-test-renderer';
-
-
+import * as React from "react";
+import { render } from "@testing-library/react";
+import { shallow } from "enzyme";
+import App from "./App";
+import renderer from "react-test-renderer";
 
 describe("App Component", () => {
-
-
   beforeEach(() => {
-    shallow(<App />, { disableLifeCycleMethods: true })
-  })
+    shallow(<App />, { disableLifeCycleMethods: true });
+  });
 
-  it('renders as expected, snapshot comparison', () => {
-    const TextInputComponent = renderer.create(<App />).toJSON()
-    expect(TextInputComponent).toMatchSnapshot()
-  })
+  it("renders as expected, snapshot comparison", () => {
+    const TextInputComponent = renderer.create(<App />).toJSON();
+    expect(TextInputComponent).toMatchSnapshot();
+  });
 
-  it('mounts the state', () => {
-    const spy = jest.spyOn(App.prototype, 'componentDidMount')
-    shallow(<App />)
-    expect(spy).toHaveBeenCalled()
-  })
+  it("mounts the state", () => {
+    const spy = jest.spyOn(App.prototype, "componentDidMount");
+    shallow(<App />);
+    expect(spy).toHaveBeenCalled();
+  });
 
-  it('calls a fetch', () => {
-    const fetchSpy = jest.spyOn(window, "fetch")
-    shallow(<App />)
-    expect(fetchSpy).toHaveBeenCalled()
-  })
-})
+  it("calls a fetch", () => {
+    const fetchSpy = jest.spyOn(window, "fetch");
+    shallow(<App />);
+    expect(fetchSpy).toHaveBeenCalled();
+  });
+});
 
-describe('createCountry', () => {
-  var app = new App()
+describe("createCountry", () => {
+  var app = new App();
   const countries = [
     {
       country_name: "USA",
@@ -45,7 +41,8 @@ describe('createCountry', () => {
       serious_critical: "8,702",
       active_cases: "311,218",
       total_cases_per_1m_population: "1,024",
-    }]
+    },
+  ];
   const state = [
     {
       countrycode: "US",
@@ -55,8 +52,8 @@ describe('createCountry', () => {
       longitude: "-82.46170658",
       confirmed: 2049,
       deaths: 44,
-    }
-  ]
+    },
+  ];
   it("creates the country array", () => {
     expect(app.createCountry(countries, state)).toEqual([
       {
@@ -70,7 +67,7 @@ describe('createCountry', () => {
         activeCases: "311,218",
         criticalCases: "8,702",
         perOneMillion: "1,024",
-        cfr: 2.86
+        cfr: 2.86,
       },
       {
         us: true,
@@ -80,13 +77,13 @@ describe('createCountry', () => {
         confirmed: "2,049",
         center: { lat: 33.8191, lng: -80.9066 },
         cfr: 2.15,
-      }])
-  })
-
-})
+      },
+    ]);
+  });
+});
 
 describe("updateUS", () => {
-  var app = new App()
+  var app = new App();
   const countries = [
     {
       country: "USA",
@@ -99,7 +96,8 @@ describe("updateUS", () => {
       activeCases: "311,218",
       criticalCases: "8,702",
       perOneMillion: "1,024",
-    }]
+    },
+  ];
   const states = [
     {
       countrycode: "US",
@@ -108,8 +106,9 @@ describe("updateUS", () => {
       latitude: "31.74847232",
       longitude: "-82.28909114",
       confirmed: 6647,
-      deaths: 211
-    }]
+      deaths: 211,
+    },
+  ];
 
   it("creates the Georgia correctly", () => {
     expect(app.updateUS(states, countries)).toEqual([
@@ -133,21 +132,21 @@ describe("updateUS", () => {
         confirmed: "6,647",
         center: { lat: 32.9866, lng: -83.6487 },
         cfr: 3.17,
-      }])
-  })
-
-})
+      },
+    ]);
+  });
+});
 
 describe("updateTotal", () => {
-  var app = new App()
+  var app = new App();
   const total = {
     total_cases: "1,250,000",
     total_deaths: "70,000",
     total_recovered: "270,000",
     new_cases: "14,000",
     new_deaths: "1000",
-    statistic_taken_at: "2020-04-06 12:53:08"
-  }
+    statistic_taken_at: "2020-04-06 12:53:08",
+  };
   it("adds the active cases and global cfr to the object", () => {
     expect(app.updateTotal(total)).toEqual({
       total_cases: "1,250,000",
@@ -157,7 +156,32 @@ describe("updateTotal", () => {
       new_deaths: "1000",
       statistic_taken_at: "2020-04-06 12:53:08",
       active_cases: "910,000",
-      globalCFR: 5.6000000000000005
-    })
-  })
-})
+      globalCFR: 5.6000000000000005,
+    });
+  });
+});
+
+describe("toInteger", () => {
+  var app = new App();
+  it("returns total cases, total deaths and total recoveries as integers", () => {
+    const totalArray = {
+      total_cases: "7,904,560",
+      new_cases: "44,036",
+      total_deaths: "432,957",
+      new_deaths: "1,039",
+      total_recovered: "4,062,496",
+      active_cases: "3,409,107",
+      deaths_per_1m_population: "55.5",
+      globalCFR: 5.477306769763276,
+      new_cases: "44,036",
+      new_deaths: "1,039",
+      serious_critical: "54,151",
+      statistic_taken_at: "2020-06-14 13:10:01",
+      total_cases: "7,904,560",
+      total_cases_per_1m_population: "1,014",
+      total_deaths: "432,957",
+      total_recovered: "4,062,496",
+    };
+    expect(app.toInteger(totalArray)).toEqual([7904560, 432957, 4062496]);
+  });
+});
