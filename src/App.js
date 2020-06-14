b0b0816a15fadb079d6d93679af33ae4c8ca411f
Asia <asiaellis5@gmail.com>
2020-06-14 15:07:55 +0100

    removed console logs

--- src/App.js
+++ src/App.js
@@ -77,7 +77,6 @@ class App extends Component {
   }
 
   fixTimeSeriesUS = (res4) => {
-    console.log("res4", res4);
     res4["USA"] = res4["US"];
     res4["S. Korea"] = res4["Korea, South"];
     res4["Taiwan"] = res4["Taiwan*"];
