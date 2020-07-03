e013c3e685e6d165f260ec631c60ab7b96e6439e
David Papamichael <davidpapamichael@gmail.com>
2020-07-03 15:04:52 +0100

    country line and country bar converted to functional components

--- src/components/graphs/country_graphs/country_data_line/country_data_line.jsx
+++ src/components/graphs/country_graphs/country_data_line/country_data_line.jsx
@@ -1,22 +1,19 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Line } from "react-chartjs-2";
 import Button from "react-bootstrap/Button";
-import GlobalCasesDoughnut from './../global_cases_doughnut/global_cases_doughnut'
+import GlobalCasesDoughnut from "./../global_cases_doughnut/global_cases_doughnut";
 
-class CountryDataLine extends Component {
+const CountryDataLine = (props) => {
+  const [doughnut, setDoughnut] = useState(false);
 
-  state = {
-    doughnut: false
-  }
-
-  createLineData = (type) => {
+  const createLineData = (type) => {
     const graphData = {
       deaths: [],
       confirmed: [],
       recoveries: [],
     };
 
-    const countryData = this.props.data[this.props.country];
+    const countryData = props.data[props.country];
 
     if (countryData !== undefined) {
       countryData.forEach((date) => {
@@ -37,163 +34,176 @@ class CountryDataLine extends Component {
     }
   };
 
-  handleDoughnutClick = () => {
-    this.setState({
-      doughnut: !this.state.doughnut
-    })
-  }
+  const handleDoughnutClick = () => {
+    setDoughnut(!doughnut);
+  };
 
-  render() {
+  const line = {
+    labels: props.createLineLabels,
+    datasets: [
+      {
+        label: "Cases",
+        data: createLineData("confirmed"),
+        fill: false,
+        backgroundColor: "#18A2B8",
+        borderColor: "#18A2B8",
+        borderWidth: 2,
+        pointBackgroundColor: "#18A2B8",
+        pointBorderColor: "#000000",
+        pointBorderWidth: 0.5,
+        pointStyle: "rectRounded",
+        pointRadius: 4,
+        pointHitRadius: 5,
+        pointHoverRadius: 5,
+        hoverBackgroundColor: "#FFFFFF",
+      },
+      {
+        label: "Deaths",
+        data: createLineData("deaths"),
+        fill: false,
+        backgroundColor: "#dc3644",
+        borderColor: "#dc3644",
+        borderWidth: 2,
+        pointBackgroundColor: "#dc3644",
+        pointBorderColor: "#000000",
+        pointBorderWidth: 0.5,
+        pointStyle: "rectRounded",
+        pointRadius: 4,
+        pointHitRadius: 5,
+        pointHoverRadius: 5,
+        hoverBackgroundColor: "#FFFFFF",
+      },
+      {
+        label: "Recoveries",
+        data: createLineData("recovered"),
+        fill: false,
+        backgroundColor: "#28a745",
+        borderColor: "#28a745",
+        borderWidth: 2,
+        pointBackgroundColor: "#28a745",
+        pointBorderColor: "#000000",
+        pointBorderWidth: 0.5,
+        pointStyle: "rectRounded",
+        pointRadius: 4,
+        pointHitRadius: 5,
+        pointHoverRadius: 5,
+        hoverBackgroundColor: "#FFFFFF",
+      },
+    ],
+  };
 
-    const line = {
-      labels: this.props.createLineLabels,
-      datasets: [
+  const options = {
+    scales: {
+      xAxes: [
         {
-          label: "Cases",
-          data: this.createLineData("confirmed"),
-          fill: false,
-          backgroundColor: "#18A2B8",
-          borderColor: "#18A2B8",
-          borderWidth: 2,
-          pointBackgroundColor: "#18A2B8",
-          pointBorderColor: "#000000",
-          pointBorderWidth: 0.5,
-          pointStyle: "rectRounded",
-          pointRadius: 4,
-          pointHitRadius: 5,
-          pointHoverRadius: 5,
-          hoverBackgroundColor: "#FFFFFF",
-        },
-        {
-          label: "Deaths",
-          data: this.createLineData("deaths"),
-          fill: false,
-          backgroundColor: "#dc3644",
-          borderColor: "#dc3644",
-          borderWidth: 2,
-          pointBackgroundColor: "#dc3644",
-          pointBorderColor: "#000000",
-          pointBorderWidth: 0.5,
-          pointStyle: "rectRounded",
-          pointRadius: 4,
-          pointHitRadius: 5,
-          pointHoverRadius: 5,
-          hoverBackgroundColor: "#FFFFFF",
-        },
-        {
-          label: "Recoveries",
-          data: this.createLineData("recovered"),
-          fill: false,
-          backgroundColor: "#28a745",
-          borderColor: "#28a745",
-          borderWidth: 2,
-          pointBackgroundColor: "#28a745",
-          pointBorderColor: "#000000",
-          pointBorderWidth: 0.5,
-          pointStyle: "rectRounded",
-          pointRadius: 4,
-          pointHitRadius: 5,
-          pointHoverRadius: 5,
-          hoverBackgroundColor: "#FFFFFF",
+          ticks: {
+            display: true,
+            major: {
+              fontStyle: "bold",
+              fontColor: "#FFFFFF",
+            },
+          },
+          gridLines: {
+            display: false,
+            drawBorder: true,
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "Date (YYYY/MM/DD)",
+            fontStyle: "bold",
+            fontColor: "#FFFFFF",
+          },
         },
       ],
-    };
-
-    const options = {
-      scales: {
-        xAxes: [
-          {
-            ticks: {
-              display: true,
-              major: {
-                fontStyle: "bold",
-                fontColor: "#FFFFFF",
-              },
-            },
-            gridLines: {
-              display: false,
-              drawBorder: true,
-            },
-            scaleLabel: {
-              display: true,
-              labelString: "Date (YYYY/MM/DD)",
+      yAxes: [
+        {
+          ticks: {
+            display: true,
+            major: {
               fontStyle: "bold",
               fontColor: "#FFFFFF",
             },
           },
-        ],
-        yAxes: [
-          {
-            ticks: {
-              display: true,
-              major: {
-                fontStyle: "bold",
-                fontColor: "#FFFFFF",
-              },
-            },
-            gridLines: {
-              display: true,
-              drawBorder: true,
-            },
-            scaleLabel: {
-              display: true,
-              labelString: "No of People",
-              fontStyle: "bold",
-              fontColor: "#FFFFFF",
-            },
+          gridLines: {
+            display: true,
+            drawBorder: true,
           },
-        ],
-      },
-      legend: {
-        display: true,
-        position: "top",
-        align: "center",
-        labels: {
-          fontSize: 12,
-          fontStyle: "bold",
-          fontColor: "#FFFFFF",
+          scaleLabel: {
+            display: true,
+            labelString: "No of People",
+            fontStyle: "bold",
+            fontColor: "#FFFFFF",
+          },
         },
+      ],
+    },
+    legend: {
+      display: true,
+      position: "top",
+      align: "center",
+      labels: {
+        fontSize: 12,
+        fontStyle: "bold",
+        fontColor: "#FFFFFF",
       },
-      lineTension: 3,
-      borderWidth: 2,
-    };
+    },
+    lineTension: 3,
+    borderWidth: 2,
+  };
 
-    return (
-      <React.Fragment>
-
-        {!this.state.doughnut && (
-          <div>
-            <h4>{`${this.props.country}`} Confirmed Cases, Deaths and Recoveries</h4>
-            <br></br>
-            <div id="description">
-              <p>
-                This represents the increase over time for confirmed cases, deaths and recoveries in {this.props.country === "United Kingdom" || this.props.country === "United Kingdom" ? `the ${this.props.country}` : `${this.props.country}`}.
-              </p>
-            </div>
-            <br></br>
-            <Button onClick={this.handleDoughnutClick} variant={"info"}>Show {`${this.props.country}`} Cases as % of Global Cases </Button>
-            <br></br>
-            <br></br>
-            <Line data={line} options={options} />
+  return (
+    <React.Fragment>
+      {!doughnut && (
+        <div>
+          <h4>{`${props.country}`} Confirmed Cases, Deaths and Recoveries</h4>
+          <br></br>
+          <div id="description">
+            <p>
+              This represents the increase over time for confirmed cases, deaths
+              and recoveries in{" "}
+              {props.country === "United Kingdom" ||
+              props.country === "United Kingdom"
+                ? `the ${props.country}`
+                : `${props.country}`}
+              .
+            </p>
           </div>
-        )}
-        {this.state.doughnut && (
-          <div>
-            <h4>{`${this.props.country}`} as % of Global Cases</h4>
-            <br></br>
-            <div id="description">
-              <p>
-                This represents {this.props.country === "United Kingdom" || this.props.country === "United Kingdom" ? `the ${this.props.country}'s` : `${this.props.country}'s`} cases as a percentage of total global cases.
-              </p>
-            </div>
-            <br></br>
-            <Button onClick={this.handleDoughnutClick} variant={"warning"}>Show Data of {`${this.props.country}`} </Button>
-            <GlobalCasesDoughnut country={this.props.country} countries={this.props.countries} total={this.props.total} />
+          <br></br>
+          <Button onClick={handleDoughnutClick} variant={"info"}>
+            Show {`${props.country}`} Cases as % of Global Cases{" "}
+          </Button>
+          <br></br>
+          <br></br>
+          <Line data={line} options={options} />
+        </div>
+      )}
+      {doughnut && (
+        <div>
+          <h4>{`${props.country}`} as % of Global Cases</h4>
+          <br></br>
+          <div id="description">
+            <p>
+              This represents{" "}
+              {props.country === "United Kingdom" ||
+              props.country === "United Kingdom"
+                ? `the ${props.country}'s`
+                : `${props.country}'s`}{" "}
+              cases as a percentage of total global cases.
+            </p>
           </div>
-        )}
-      </React.Fragment>
-    );
-  }
-}
+          <br></br>
+          <Button onClick={handleDoughnutClick} variant={"warning"}>
+            Show Data of {`${props.country}`}{" "}
+          </Button>
+          <GlobalCasesDoughnut
+            country={props.country}
+            countries={props.countries}
+            total={props.total}
+          />
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
 
 export default CountryDataLine;
