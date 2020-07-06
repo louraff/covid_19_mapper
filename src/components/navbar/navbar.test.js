f8aa1078e0c6000a67601168f4d1196077ab8660
David Papamichael <davidpapamichael@gmail.com>
2020-07-06 17:43:32 +0100

    component tests for Navbar and Searchbar passed

--- src/components/navbar/navbar.test.js
+++ src/components/navbar/navbar.test.js
@@ -1,15 +1,56 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-import { shallow } from 'enzyme';
-import Header from './navbar'
-import renderer from 'react-test-renderer';
+import * as React from "react";
+import { shallow } from "enzyme";
+import Header from "./navbar";
+import renderer from "react-test-renderer";
 
-describe("Navbar Component", () => {
-  it('does not crash when loaded onto page', () => {
-    shallow(<Header total={[]} />)
-  })
-  it('render correctly Header component', () => {
-    const TextInputComponent = renderer.create(<Header total={[]} globalCFR={[2.9587486]} totalInt={[]} countries={[]} integerCountries={[]} />).toJSON();
-    expect(TextInputComponent).toMatchSnapshot();
+describe("Navbar", () => {
+  let wrapper;
+
+  beforeEach(
+    () =>
+      (wrapper = shallow(
+        <Header
+          total={[]}
+          globalCFR={[]}
+          totalInt={[]}
+          countries={[]}
+          integerCountries={[]}
+        />
+      ))
+  );
+
+  it("should render a <div />", () => {
+    expect(wrapper.find("div").length).toEqual(20);
   });
-})
+
+  it("should render a <img />", () => {
+    expect(wrapper.find("img").length).toEqual(6);
+  });
+
+  it("should render a <Navbar />", () => {
+    expect(wrapper.find("Navbar").length).toEqual(1);
+  });
+
+  it("should render a <Nav />", () => {
+    expect(wrapper.find("Nav").length).toEqual(6);
+  });
+
+  it("should render a <DropdownButton />", () => {
+    expect(wrapper.find("DropdownButton").length).toEqual(6);
+  });
+
+  it("renders correctly", () => {
+    const component = renderer
+      .create(
+        <Header
+          total={[]}
+          globalCFR={[]}
+          totalInt={[]}
+          countries={[]}
+          integerCountries={[]}
+        />
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+});
--- src/components/searchbar/searchbar.test.js
+++ src/components/searchbar/searchbar.test.js
@@ -1,11 +1,28 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-import { shallow } from 'enzyme';
-import SearchContainer from './searchbar'
-import renderer from 'react-test-renderer';
+import * as React from "react";
+import { shallow } from "enzyme";
+import SearchContainer from "./searchbar";
+import CountryGraphContainer from "../graphs/country_graphs/country_graph";
 
-describe("Navbar Component", () => {
-  it('does not crash when loaded onto page', () => {
-    shallow(<SearchContainer total={[]} />)
-  })
-})
+describe("Searchbar", () => {
+  let wrapper;
+
+  beforeEach(() => (wrapper = shallow(<SearchContainer />)));
+
+  it("should render a <div />", () => {
+    expect(wrapper.find("div").length).toEqual(3);
+  });
+
+  it("should render a <form />", () => {
+    expect(wrapper.find("form").length).toEqual(1);
+  });
+
+  it("should render the CountryGraphContainer Component", () => {
+    expect(wrapper.containsMatchingElement(<CountryGraphContainer />)).toEqual(
+      true
+    );
+  });
+
+  it("renders correctly", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
